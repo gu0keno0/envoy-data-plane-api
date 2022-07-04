6ed56886dfb36064745a160efc88ebc2f269af0b
Jeff Jiang <jefjiang@jefjiang-ld1.linkedin.biz>
2022-07-04 07:36:29 -0700

    fix import path of cncf annotations package

--- gen-golang.sh
+++ gen-golang.sh
@@ -13,3 +13,5 @@ git clone https://github.com/prometheus/client_model.git ./prometheus_client_mod
 popd
 
 protoc --proto_path=./proto-deps/golang-protobuf --proto_path=./proto-deps/prometheus_client_model --proto_path=./proto-deps/opentelemetry-proto --proto_path=./proto-deps/opencensus-proto/src --proto_path=./proto-deps/protoc-gen-validate --proto_path=./proto-deps/cncf-xds --proto_path=./proto-deps/googleapis --proto_path=. --validate_out="lang=go:./proto-gen-golang" --go_out=plugins=grpc:./proto-gen-golang $(find ./ -iname "*proto" | grep -v './proto-deps')
+
+find ./proto-gen-golang/ -name "*.go" | xargs -n1 -I{} sed -i 's/github.com\/cncf\/xds\/go\/annotations/github.com\/cncf\/xds\/go\/udpa\/annotations/g' {}
