87a25cb3cf3681e4a79b7c6bc1e5521b0ef43eb6
Jeff Jiang <jefjiang@jefjiang-ld1.linkedin.biz>
2022-07-11 14:21:15 -0700

    adjustments to ./contrib folder

--- gen-golang.sh
+++ gen-golang.sh
@@ -14,4 +14,16 @@ popd
 
 protoc --proto_path=./proto-deps/golang-protobuf --proto_path=./proto-deps/prometheus_client_model --proto_path=./proto-deps/opentelemetry-proto --proto_path=./proto-deps/opencensus-proto/src --proto_path=./proto-deps/protoc-gen-validate --proto_path=./proto-deps/cncf-xds --proto_path=./proto-deps/googleapis --proto_path=. --validate_out="lang=go:./proto-gen-golang" --go_out=plugins=grpc:./proto-gen-golang $(find ./ -iname "*proto" | grep -v './proto-deps')
 
+mkdir ./proto-gen-golang/contrib
+mkdir ./proto-gen-golang/github.com/envoyproxy/go-control-plane/contrib
+
+protoc --proto_path=./proto-deps/golang-protobuf --proto_path=./proto-deps/prometheus_client_model --proto_path=./proto-deps/opentelemetry-proto --proto_path=./proto-deps/opencensus-proto/src --proto_path=./proto-deps/protoc-gen-validate --proto_path=./proto-deps/cncf-xds --proto_path=./proto-deps/googleapis --proto_path=. --validate_out=lang=go:./proto-gen-golang/contrib --go_out=plugins=grpc:./proto-gen-golang/contrib $(find ./contrib/ -iname "*proto" | grep -v './proto-deps')
+
+mv  ./proto-gen-golang/contrib/github.com/envoyproxy/go-control-plane/envoy ./proto-gen-golang/github.com/envoyproxy/go-control-plane/contrib/envoy
+rm -rf ./proto-gen-golang/contrib
+
+find ./proto-gen-golang/github.com/envoyproxy/go-control-plane/contrib -name "*.go" | xargs -n1 -I{} sed -i 's/github.com\/envoyproxy\/go-control-plane\/envoy\/extensions/github.com\/envoyproxy\/go-control-plane\/contrib\/envoy\/extensions/g' {}
+
+find ./proto-gen-golang/github.com/envoyproxy/go-control-plane/contrib -name "*.go" | xargs -n1 -I{} sed -i 's/github.com\/envoyproxy\/go-control-plane\/contrib\/envoy\/extensions\/transport_sockets/github.com\/envoyproxy\/go-control-plane\/envoy\/extensions\/transport_sockets/g' {}
+
 find ./proto-gen-golang/ -name "*.go" | xargs -n1 -I{} sed -i 's/github.com\/cncf\/xds\/go\/annotations/github.com\/cncf\/xds\/go\/udpa\/annotations/g' {}
